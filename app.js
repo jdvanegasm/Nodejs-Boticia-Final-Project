6fa27e20b82c2ffce8a7a34c287c1972d12d5cef
cesarSPC <cesaraupc@gmail.com>
2023-11-25 10:07:17 -0500

    Declaración de swagger

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const mongoose = require('mongoose'); //Requiere mongoose para conectarse a la b
 const express = require('express'); //La constante express importa la libreria por medio del require
 const app = express(); //La constante app settea el plugin de conexion express
 const port = 3000; //Puerto en el que se ejecuta el servidor
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 require('dotenv').config();
 
+const swaggerSpec = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "API Proyecto ACM",
+        version: "1.0.0"
+      },
+      servers: [
+        {
+          url: "http://localhost:3000"
+        }
+      ],
+    },
+    apis: [`${path.join(__dirname, "./controllers/*.js")}`] 
+}
 // Rutas - Ejemplos (End points iniciales)
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-// Controladores para las colecciones 'user' y 'userType'
-app.use(require('./controllers/userController'));
-app.use(require('./controllers/userTypeController'));
 
 //Funcion que conecta a la DB usando la URL del sevidor de mongoose
 mongoose.connect('mongodb+srv://JDVM:EY85oVuSZkVjYxhk@cluster0.p2ovi4c.mongodb.net/BoticiaDB?retryWrites=true&w=majority')
@@ -24,4 +38,11 @@ app.listen(port,()=>{
     console.log(`Servidor en ejecucion en http://localhost:${port}`); 
     //Impresion en pantalla con el link standar del localhost junto con el puerto 
 })
+
+app.use(
+    '/api', 
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerJsDoc(swaggerSpec))
+);
+
 app.use(require('./controllers/routes'));
